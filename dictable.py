5e0e5327dbaa643693c4d854d3a10813d3ac171f
David Weinberg <davidweinberg0@gmail.com>
2016-12-03 23:08:08 +0200

    dictable

---
+++ dictable.py
@@ -0,0 +1,14 @@
+class Dictable:
+    def dict(self):
+        members = [attr for attr in list(set(dir(self))-set(dir(self.__class__)))
+                   if not callable(attr) and not attr.startswith("_")]
+        d = {}
+        for m in members:
+            v = getattr(self, m)
+            if isinstance(v, Dictable):
+                d[m] = v.dict()
+            elif "__dict__" in dir(v):
+                d[m] = v.__dict__
+            else:
+                d[m] = v
+        return d
